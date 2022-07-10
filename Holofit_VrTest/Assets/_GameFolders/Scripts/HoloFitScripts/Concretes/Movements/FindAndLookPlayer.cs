0efe35924908fbe0cbcbb512a14fe2aaae1b7512
berkterek <42406263+berkterek@users.noreply.github.com>
2022-07-10 22:47:43 +0300

    refactor: Target canvas look and move player position

--- Holofit_VrTest/Assets/_GameFolders/Scripts/HoloFitScripts/Concretes/Movements/FindAndLookPlayer.cs
+++ Holofit_VrTest/Assets/_GameFolders/Scripts/HoloFitScripts/Concretes/Movements/FindAndLookPlayer.cs
@@ -6,6 +6,9 @@ namespace HoloFit_VrTest.Movements
 {
     public class FindAndLookPlayer : MonoBehaviour
     {
+        [SerializeField] Transform _parentTransform;
+        [SerializeField] float _maxDistance = 0.5f;
+        [SerializeField] float _moveSpeed = 1f;
         Transform _playerTranform;
         Transform _thisTransform;
 
@@ -24,6 +27,33 @@ namespace HoloFit_VrTest.Movements
         }
 
         void Update()
+        {
+            RotationProcess();
+            MoveProcess();
+        }
+
+        void MoveProcess()
+        {
+            Vector3 parentPositionWithY = new Vector3(_parentTransform.position.x, 0f, _parentTransform.position.z);
+            Vector3 thisTransformWithY = new Vector3(_thisTransform.position.x, 0f, _thisTransform.position.z);
+            bool maxDistanceResult = Vector3.Distance(parentPositionWithY, thisTransformWithY) < _maxDistance;
+
+            float moveSpeed = Time.deltaTime * _moveSpeed;
+            float yPosition = _thisTransform.position.y;
+            
+            if(maxDistanceResult)
+            {
+                _thisTransform.position = Vector3.MoveTowards(_thisTransform.position, _playerTranform.position, moveSpeed);
+            }
+            else
+            {
+                _thisTransform.position = Vector3.MoveTowards(_thisTransform.position, _parentTransform.position, moveSpeed);
+            }
+
+            _thisTransform.position = new Vector3(_thisTransform.position.x, yPosition, _thisTransform.position.z);
+        }
+
+        private void RotationProcess()
         {
             _thisTransform.LookAt(_playerTranform.position);
             Vector3 eulerAngles = _thisTransform.eulerAngles;
